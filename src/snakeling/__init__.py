5e8a2d77b53ede6cd2c5aa315a4c730847090862
Nicolas Canceill <nicolas.canceill@gmail.com>
2023-11-11 22:22:19 +0100

    fix package name

--- src/snakeling/__init__.py
+++ src/snakeling/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2023 Nicolas Canceill
-# This file is part of the `elliptic` package
+# This file is part of the `snakeling` package
 # GNU General Public License v3.0+
 # See COPYING.md or https://www.gnu.org/licenses/
 
